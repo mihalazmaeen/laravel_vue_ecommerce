0b156758417f2fa4ee5cff89dce2398504f369ca
mihalazmaeen <azmaeenmihal1@gmail.com>
2022-12-26 19:27:23 +0600

    installed vue router

---
+++ backend/src/store/index.js
@@ -0,0 +1,12 @@
+import {createStore} from "vuex";
+
+const store= createStore({
+    state:{
+        test:'12345'
+    },
+    getters:{},
+    actions:{},
+    mutators:{},
+})
+export default store
+
